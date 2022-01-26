ff689e7a5e7e4c112dd7762af4d6b4e4e30e3887
LouisPotterie <38073253+LouisPotterie@users.noreply.github.com>
2022-01-26 15:35:34 +0100

    Add files via upload
    first commit

---
+++ CryptoCompareProject/CryptoCompareProject/ApiCall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace CryptoCompareProject
+{
+    public class ApiCall
+    {
+        public string ApiCallJSON(string address)
+        {
+            WebClient webClient = new WebClient();
+            var jsonString = webClient.DownloadString(address);
+            return jsonString;
+        }
+
+        public HistoricalData.DataRoot HistoricalDataStorage(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<HistoricalData.DataRoot>(jsonString);
+        }
+
+        public List<double> ClosePriceList(HistoricalData.DataRoot historicalDataStorage)
+        {
+            List<double> closesList = new List<double>();
+            
+            for (int i = 0; i < historicalDataStorage.Data.Data.Count; i++)
+            {
+                closesList.Add(historicalDataStorage.Data.Data[i].close);
+            }
+            
+            return closesList;
+        }
+    }
+}
---
+++ CryptoCompareProject/CryptoCompareProject/HistoricalData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoCompareProject
+{
+    public class HistoricalData
+    {
+        // 1er niveau de profondeur de la Structure Json 
+        public class DataRoot {
+            public string Response {get; set; }
+            public string Type {get; set; }
+            public bool Aggregated{get; set; }
+            public JsonData Data {get; set;}
+        }
+
+        // 2ème niveau de profondeur de la Structure Json 
+        public class JsonData  {
+            public IList<HistoricalCrypto> Data {get; set;}
+        }
+    
+        // 3ème niveau de profondeur de la Structure Json 
+
+        public class HistoricalCrypto
+        {
+            public string time; 
+            public float high;
+            public float low;
+            public float open;
+            public float volumefrom;
+            public float volumeto;
+            public float close;
+        }
+    }
+}
